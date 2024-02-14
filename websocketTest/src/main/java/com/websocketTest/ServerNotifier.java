d9ef43a5bd72b7b15b81db477c924d2c1d4f2139
Narendra Maurya <mauryanarendra2003@gmail.com>
2024-02-14 20:00:07 +0530

    UI fix

--- websocketTest/src/main/java/com/websocketTest/ServerNotifier.java
+++ websocketTest/src/main/java/com/websocketTest/ServerNotifier.java
@@ -12,7 +12,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-
 @Component
 @ServerEndpoint(value = "/websocket")
 public class ServerNotifier implements Notifier {
